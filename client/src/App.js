763d99402481d0ccbface33ebb6719716116a25d
Manishdevaraj <117499288+Manishdevaraj@users.noreply.github.com>
2024-07-26 15:14:29 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Signup from "./Pages/Signup";
+import Login from "./Pages/Login";
+
+function App() {
+
+	return (
+		<Routes>
+			
+			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+		</Routes>
+	);
+}
+
+export default App;
